3fe581751594b6b637fb7fd10779ab2ed260cec8
OM_JAISWAL <omjaiswal1802@gmail.com>
2026-02-06 22:11:50 +0530

    Create first.js

---
+++ first.js
@@ -0,0 +1,15 @@
+console.log("WELCOME TO REALITY!!");
+console.log("START YOUR LIFE");
+let fullname="Shin Kamiji";
+console.log("NAME:",fullname);
+let age=20;
+console.log("AGE:",age);
+const PI=3.14;
+console.log("22/7=",PI);
+const product={
+    pname:"Parker Jotter Standard CT Ball Pen(Black)",
+    stars:4,
+    price:270,
+    discount:"5% off"
+};
+console.log(product);
