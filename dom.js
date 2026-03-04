602054dba7ad1140574f8443b7ffba1e6ac26799
OM_JAISWAL <omjaiswal1802@gmail.com>
2026-03-04 11:31:12 +0530

    Add HTML and CSS comments to dom.js
    Added HTML and CSS comments to the top of the file for clarity.

--- dom.js
+++ dom.js
@@ -1,3 +1,5 @@
+/*
+//HTML SCRIPT
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -28,4 +30,47 @@
 </html>
 
 
+//CSS SCRIPT
+#heading{
+    color:red;
+    background-color: beige;
+}
+.heading{
+    color:rgb(35, 21, 158);
+    background-color: rgb(61, 221, 13);
+}
+body{
+    background-color: aqua;
+}
+h1{
+    background-color: blue;
+}
+*/
 //JS SCRIPT
+let div=document.querySelector("div");
+console.log(div.innerText);
+console.log(div.innerHTML);
+
+console.log(document.body);
+console.dir(document.body);
+//document.body.style.background = "green";
+let heading=document.getElementById("heading");
+console.dir(heading);
+console.log(heading);
+
+let heading2=document.getElementsByClassName("heading");
+console.dir(heading2);
+console.log(heading2);
+
+let para=document.getElementsByTagName("p");
+console.log(para);
+console.dir(para);
+
+let felements=document.querySelector("p");
+console.dir(felements);
+
+let allelements=document.querySelectorAll("p");
+console.dir(allelements);
+
+let classElements=document.querySelector(".heading");
+console.dir(classElements)
