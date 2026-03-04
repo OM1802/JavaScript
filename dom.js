ae52469a99c2b47e77c88b7022fba8764accfe76
OM_JAISWAL <omjaiswal1802@gmail.com>
2026-03-04 11:28:52 +0530

    Create dom.js

---
+++ dom.js
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>JS</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div>
+        <h1>FRUITS</h1>
+        <ul>
+            <li>ORANGE</li>
+            <li>WATERMELON</li>
+            <li>MANGO</li>
+        </ul>
+    </div>
+    <h1>TEST TEXT</h1>
+    <h1 id="heading">DOM CONCEPTS</h1>
+    <h4 class="heading">TOPIC: Starter Code</h4>
+    <h4 class="heading">TOPIC: Starter Code</h4>
+    <h4 class="heading">TOPIC: Starter Code</h4>
+    <p>Learning about DOM's conecepts</p>
+    <p>Learning about DOM's conecepts</p>
+    <button>CLICK HERE!!</button>
+<script src="dom.js"></script>
+</body>
+</html>
+
+
+//JS SCRIPT
